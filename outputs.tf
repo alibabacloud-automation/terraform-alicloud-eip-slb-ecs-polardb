ce86e9c0301cf1b7e82cb2e951229c9a22486417
shanye <shanye.xzq@alibaba-inc.com>
2025-03-14 11:06:45 +0800

    examples/complete: improve the examples

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,34 @@
 output "this_ecs_id" {
-  value = alicloud_instance.default.id
+  description = "The ID of the ECS instance."
+  value       = alicloud_instance.default.id
 }
 
 output "this_ecs_name" {
-  value = alicloud_instance.default.instance_name
+  description = "The name of the ECS instance."
+  value       = alicloud_instance.default.instance_name
 }
 
 output "this_polardb_database_id" {
-  value = alicloud_polardb_database.default.id
+  description = "The ID of the PolarDB database."
+  value       = alicloud_polardb_database.default.id
 }
 
 output "this_polardb_database_name" {
-  value = alicloud_polardb_database.default.db_name
+  description = "The name of the PolarDB database."
+  value       = alicloud_polardb_database.default.db_name
 }
 
 output "this_slb_id" {
-  value = alicloud_slb_load_balancer.default.id
+  description = "The ID of the SLB instance."
+  value       = alicloud_slb_load_balancer.default.id
 }
 
 output "this_slb_name" {
-  value = alicloud_slb_load_balancer.default.load_balancer_name
+  description = "The name of the SLB instance."
+  value       = alicloud_slb_load_balancer.default.load_balancer_name
 }
 
 output "this_eip_id" {
-  value = alicloud_eip_address.default.id
+  description = "The ID of the EIP instance."
+  value       = alicloud_eip_address.default.id
 }
