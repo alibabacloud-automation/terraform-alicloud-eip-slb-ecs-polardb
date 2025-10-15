45659d748ccdb4258fa73228a8f7c48b3a395306
shanye <shanye.xzq@alibaba-inc.com>
2025-10-15 19:07:11 +0800

    fix: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,22 +1,30 @@
+provider "alicloud" {
+  region = "ap-southeast-5"
+}
 data "alicloud_polardb_zones" "default" {
 }
 
+locals {
+  zone_id = data.alicloud_polardb_zones.default.zones[0].id
+}
+
 data "alicloud_images" "default" {
-  name_regex = "^centos_6"
+  most_recent   = true
+  instance_type = data.alicloud_instance_types.default.instance_types[0].id
 }
 
 data "alicloud_instance_types" "default" {
-  availability_zone    = data.alicloud_polardb_zones.default.zones[0].id
+  availability_zone    = local.zone_id
   cpu_core_count       = 2
   memory_size          = 8
-  instance_type_family = "ecs.g6"
+  instance_type_family = "ecs.g9i"
 }
 
 data "alicloud_polardb_node_classes" "default" {
   pay_type   = var.pay_type
   db_type    = "MySQL"
   db_version = "5.6"
-  zone_id    = data.alicloud_polardb_zones.default.zones[0].id
+  zone_id    = local.zone_id
 }
 
 module "vpc" {
@@ -28,7 +36,7 @@ module "vpc" {
   vpc_cidr           = "172.16.0.0/16"
   vswitch_name       = var.name
   vswitch_cidrs      = ["172.16.0.0/21"]
-  availability_zones = [data.alicloud_polardb_zones.default.zones[0].id]
+  availability_zones = [local.zone_id]
 }
 
 module "security_group" {
@@ -55,17 +63,17 @@ module "example" {
   slb_tags_info    = var.slb_tags_info
 
   #alicloud_instance
-  availability_zone          = data.alicloud_polardb_zones.default.zones[0].id
+  availability_zone          = local.zone_id
   security_group_ids         = [module.security_group.this_security_group_id]
   instance_type              = data.alicloud_instance_types.default.instance_types[0].id
-  system_disk_category       = "cloud_efficiency"
+  system_disk_category       = "cloud_essd"
   system_disk_name           = var.system_disk_name
   system_disk_description    = var.system_disk_description
-  image_id                   = data.alicloud_images.default.images[0].id
+  image_id                   = data.alicloud_images.default.images[0].image_id
   internet_max_bandwidth_out = var.internet_max_bandwidth_out
   ecs_size                   = 1200
   data_disks_name            = "data_disks_name"
-  category                   = "cloud_efficiency"
+  category                   = "cloud_essd"
   description                = "tf-vpc-ecs-description"
   encrypted                  = true
 
